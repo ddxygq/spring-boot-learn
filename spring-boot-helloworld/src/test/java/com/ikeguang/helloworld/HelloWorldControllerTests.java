4f663929c1f4dd4c62ad4ba257efef87ff1a3df7
keguang <keguang@2144.cn>
2019-06-20 09:26:13 +0800

    添加spring-boot-web

---
+++ spring-boot-helloworld/src/test/java/com/ikeguang/helloworld/HelloWorldControllerTests.java
@@ -0,0 +1,41 @@
+package com.ikeguang.helloworld;
+
+import com.ikeguang.helloworld.controller.HelloWorldController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+/**
+ * @ Author: keguang
+ * @ Date: 2019/6/19 20:12
+ * @ version: v1.0.0
+ * @ description:
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloWorldControllerTests {
+
+    private MockMvc mvc;
+    @Before
+    public void setUp(){
+        mvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
+}
